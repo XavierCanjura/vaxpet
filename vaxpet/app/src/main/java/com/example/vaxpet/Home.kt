3676e8551f3a94398376578918c58bdccff9802b
RubidiaPaz <rubigarcia190@gmail.com>
2023-04-16 13:23:40 -0600

    Correccion de Layouts, creacion de perfil de mascota y listado

--- vaxpet/app/src/main/java/com/example/vaxpet/Home.kt
+++ vaxpet/app/src/main/java/com/example/vaxpet/Home.kt
@@ -5,11 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+
 
 /**
  * A simple [Fragment] subclass.
@@ -36,6 +39,7 @@ class Home : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
+
 
     companion object {
         /**
--- vaxpet/app/src/main/java/com/example/vaxpet/MainActivity.kt
+++ vaxpet/app/src/main/java/com/example/vaxpet/MainActivity.kt
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.example.vaxpet.databinding.ActivityMainBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    val fab = findViewById<FloatingActionButton>(R.id.fab)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -21,10 +24,10 @@ class MainActivity : AppCompatActivity() {
                 R.id.citas -> replaceFragment(Citas())
                 R.id.perfil -> replaceFragment(Perfil())
             }
-
             true
         }
     }
+
 
     // Funcion que nos ayudara hacer el cambio de pantallas cuando se seleccione en el nav
     private fun replaceFragment(fragment: Fragment){
