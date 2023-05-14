5f408a8b641612dfe4e3f4316e37929b3c374fb6
ggsgranados <67867979+ggsgranados@users.noreply.github.com>
2023-05-14 08:26:48 -0600

    Agregado Inicio de Sesion con Google

--- vaxpet/app/src/main/java/com/example/vaxpet/Perfil.kt
+++ vaxpet/app/src/main/java/com/example/vaxpet/Perfil.kt
@@ -1,5 +1,6 @@
 package com.example.vaxpet
 
+
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -8,7 +9,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import androidx.core.view.isVisible
 import com.example.desafiopractico2.usuarioData
+import com.google.android.material.imageview.ShapeableImageView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 
@@ -21,6 +24,7 @@ class Perfil : Fragment() {
     private lateinit var auth: FirebaseAuth
     private lateinit var currentUserID: String
     private lateinit var txtUsuario: TextView
+    private lateinit var imgUsuario: ShapeableImageView
 
 
 
@@ -38,6 +42,7 @@ class Perfil : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_perfil, container, false)
         txtUsuario = view.findViewById<View>(R.id.txtUsuario) as TextView
+        imgUsuario = view.findViewById<View>(R.id.imgUsuario) as ShapeableImageView
 
 
         // Inicializar la instancia de Firebase Auth y obtener el ID del usuario actual
@@ -64,6 +69,15 @@ class Perfil : Fragment() {
                         sb.append("$apellidos")
 
                         txtUsuario.text = sb.toString()
+                        val btnEditarCuenta = view.findViewById<View>(R.id.btnEditarCuenta) as Button
+                        btnEditarCuenta.isVisible = true
+                        break
+                    }
+                    else{
+                        txtUsuario.text = auth.currentUser!!.displayName
+                        val btnEditarCuenta = view.findViewById<View>(R.id.btnEditarCuenta) as Button
+                        btnEditarCuenta.isVisible = false
+
                     }
                 }
         }
@@ -88,9 +102,13 @@ class Perfil : Fragment() {
         //Asigna listener para cerrar sesion.
         btnCerrarSesion.setOnClickListener{ view: View ->
 
-            FirebaseAuth.getInstance().signOut().also {
+            auth.signOut().also {
 
                 getActivity()?.onBackPressed()
+                activity?.finish()
+                val intent = Intent (activity , LoginActivity::class.java)
+                activity?.startActivity(intent)
+
 
             }
         }
